53b9ccd1e167226f1e78079ae5bb450c7e9b3cee
czh50501607 <20719czh@sina.com>
2020-10-11 09:10:54 +0800

    完善基础内容

--- bin/command.js
+++ bin/command.js
@@ -1,13 +1,26 @@
+// 接收node 命令的 commander 插件
 const program = require('commander');
+// 让终端字符串变得有颜色
+const chalk = require('chalk');
 
 const package = require('../package');
-
-console.log(package);
 
 program
   .version(package.version) //  显示版本号
   .usage('<command> [options]');
 
 program.command('list').description('显示所有模板').action(require('../lib/list'));
+
 program.command('create').description('生成一个项目').action(require('../lib/create'));
 
+/**
+ * help ----
+ */
+program.on('--help', () => {
+  console.log();
+  console.log(` Run ${chalk.cyan(`czh-cli <command> --help`)} for detailed usage of given command.`);
+  console.log();
+});
+
+// ! 这块非常重要必写，前面是定义，这里是解析命令行里的参数
+program.parse(process.argv);
